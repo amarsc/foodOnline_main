d9fa47bc12c2a1f5d00b2972523ce9618f973984
amarsc <amarchauhan2804@gmail.com>
2024-03-26 23:58:29 +0530

    menu fooditems by category added

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from menu.models import Category
+from django.shortcuts import get_object_or_404, render
 
 from vendor.models import Vendor
-
+from django.db.models import Prefetch
 # Create your views here.
 
 def marketplace(request):
@@ -16,4 +17,16 @@ def marketplace(request):
 
 
 def vendor_detail(request,vendor_slug):
-    return render(request,'marketplace/vendor_detail.html')
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset= FoodItem.objects.filter(is_available=True)
+        )
+    )
+    context = {
+        'vendor': vendor,
+        'categories': categories,
+    }
+
+    return render(request,'marketplace/vendor_detail.html',context)
